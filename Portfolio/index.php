11f8d9111083e5ab7bd506d59ab8892b43986e29
Mohamad Adam <117719050+MohamadAdam@users.noreply.github.com>
2023-11-20 12:13:36 +0200

    Add files via upload

---
+++ Portfolio/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mohamad Portfolio</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css" >
+</head>
+<body>
+   
+    <section id="title" class="gradient-background">
+    <nav class="navbar navbar-expand-lg navbar-dark m-0 p-0 bg-black">
+    <div class="container p-0 ">
+      <a class="navbar-brand" href="#"><img src="./img/m-a-ma-initial-letter-logo-design-template-vector-30018267-removebg-preview.png" width="70" height="60"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+        <ul class="navbar-nav gap-5">
+          <li class="nav-item">
+            <a class="nav-link text-white fs-5" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white fs-5" href="#">About Me</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white fs-5" href="#">Projects</a>
+          </li>
+        </ul>
+      </div>
+      <div class="navbar-nav">
+       <button type="button" class="btn btn-outline-success  text-white fs-6">Contact Me
+       </button></div>
+    </div>
+  </nav>
+  
+  <div class="container col-9 col-xxl-8 col-sm-5 col-md-4 px-1 py-1 ">
+    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+      <div class="col-5 col-sm-10 col-lg-6">
+        <img src="./img/Mohamad-removebg.png" class="d-block mx-lg-auto  rounded-circle " width="300">
+      </div>
+      <div class="col-lg-6 justify-content-sm-center align-items-center">
+        <h1 class="display-4 fw-bold text-body-emphasis  lh-1 mb-3">Hello,I'm Mohamad Adam</h1>
+        <p class=" fs-3"> Full stuck Web Developer.</p>
+        <div class="d-grid  d-md-flex justify-content-md-start">
+          <button type="button" class="btn bg-black text-white btn-lg px-4 me-md-2 ">More About Me
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"></path>
+</svg></button>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+  </section>
+
+</body>
+</html>
